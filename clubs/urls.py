3c14b1191410e67ed67505a8555fa71202396ef3
Fernando <fernandotos@gmail.com>
2026-09-03 13:34:34 -0300

    UI: Implementa página de detalhes do ranking com tabela de pontos e filtro de rodadas dinâmico

--- clubs/urls.py
+++ clubs/urls.py
@@ -6,4 +6,5 @@ app_name = 'clubs'
 urlpatterns = [
     path('', views.club_list, name='list'),
     path('<int:club_id>/', views.club_detail, name='detail'),
+    path('<int:club_id>/ranking/<int:ranking_id>/', views.ranking_detail, name='ranking_detail'),
 ]
--- clubs/views.py
+++ clubs/views.py
@@ -10,3 +10,27 @@ def club_detail(request, club_id):
     rankings = club.tournaments.filter(is_active=True, tournament_type='ranking')
     knockouts = club.tournaments.filter(is_active=True, tournament_type='knockout')
     return render(request, 'club_detail.html', {'club': club, 'rankings': rankings, 'knockouts': knockouts})
+
+def ranking_detail(request, club_id, ranking_id):
+    club = get_object_or_404(Club, id=club_id)
+    ranking = get_object_or_404(club.tournaments, id=ranking_id, tournament_type='ranking', is_active=True)
+    
+    categories = ranking.categories.all()
+    
+    data = {}
+    for cat in categories:
+        players = cat.players.all()
+        matches = cat.matches.all().order_by('round_number')
+        
+        rounds_dict = {}
+        for m in matches:
+            if m.round_number not in rounds_dict:
+                rounds_dict[m.round_number] = []
+            rounds_dict[m.round_number].append(m)
+            
+        data[cat] = {
+            'players': players,
+            'rounds': rounds_dict
+        }
+        
+    return render(request, 'ranking_detail.html', {'club': club, 'ranking': ranking, 'data': data})
