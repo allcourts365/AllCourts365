9705a39b9e2dd73436b364c5cd6cd0bd28082ca0
Fernando <fernandotos@gmail.com>
2026-09-16 20:01:37 -0300

    refactor: change url routes from ranking to season and torneio to eliminatory

--- clubs/urls.py
+++ clubs/urls.py
@@ -7,9 +7,9 @@ app_name = 'clubs'
 urlpatterns = [
     path('', views.club_list, name='list'),
     path('<int:club_id>/', views.club_detail, name='detail'),
-    path('<int:club_id>/ranking/<int:ranking_id>/', views.ranking_detail, name='ranking_detail'),
-    path('<int:club_id>/torneio/<int:tournament_id>/', views.knockout_detail, name='knockout_detail'),
-    path('<int:club_id>/torneio/<int:tournament_id>/categoria/<int:category_id>/', views.knockout_bracket, name='knockout_bracket'),
+    path('<int:club_id>/season/<int:ranking_id>/', views.ranking_detail, name='ranking_detail'),
+    path('<int:club_id>/eliminatory/<int:tournament_id>/', views.knockout_detail, name='knockout_detail'),
+    path('<int:club_id>/eliminatory/<int:tournament_id>/categoria/<int:category_id>/', views.knockout_bracket, name='knockout_bracket'),
     path('<int:club_id>/ranking-eliminatorio/', views.knockout_general_ranking, name='knockout_general_ranking'),
     path('download/modelo-torneio/', views.download_knockout_template, name='download_knockout_template'),
     # Noticias por clube
