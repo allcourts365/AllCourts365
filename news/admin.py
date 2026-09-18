f2540cbd959283a7decf38531abcdd3a3aa4c004
Fernando <fernandotos@gmail.com>
2026-09-18 08:44:41 -0300

    Revert admin custom CSS that broke changelist layout

--- news/admin.py
+++ news/admin.py
@@ -15,11 +15,6 @@ class NewsAdminForm(forms.ModelForm):
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    class Media:
-        css = {
-            'all': ('css/admin/custom_changelist.css',)
-        }
-        
     form = NewsAdminForm
     list_display = ("title", "club_display", "author", "is_published", "published_at", "duplicate_button")
     list_filter = ("is_published", "club")
@@ -134,11 +129,6 @@ from django.contrib.auth.models import User
 
 @admin.register(BroadcastMessage)
 class BroadcastMessageAdmin(admin.ModelAdmin):
-    class Media:
-        css = {
-            'all': ('css/admin/custom_changelist.css',)
-        }
-
     list_display = ('subject', 'club_display', 'sender', 'created_at', 'duplicate_button')
     list_filter = ('is_global', 'club')
     search_fields = ('subject', 'body')
